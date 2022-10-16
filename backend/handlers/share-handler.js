be359cbbad72dadf9f2e243c10769798ce8a4889
Giwan Kim <giwankim@gmail.com>
2022-10-16 22:03:49 +0900

    feat: add middy, powertools, and content negotiation

--- backend/handlers/share-handler.js
+++ backend/handlers/share-handler.js
@@ -1,15 +1,44 @@
 import httpStatus from 'http-status'
+import middy from '@middy/core'
+import httpContentNegotiation from '@middy/http-content-negotiation'
+import httpHeaderNormalizer from '@middy/http-header-normalizer'
+import { randomUUID } from 'node:crypto'
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
-import { randomUUID } from 'node:crypto'
+import {
+  Metrics,
+  logMetrics,
+  MetricUnits
+} from '@aws-lambda-powertools/metrics'
+import { Tracer, captureLambdaHandler } from '@aws-lambda-powertools/tracer'
+import { Logger, injectLambdaContext } from '@aws-lambda-powertools/logger'
 
 const { BUCKET_NAME, BASE_URL } = process.env
 const EXPIRY_DEFAULT = 24 * 60 * 60 // 1 day
 const MIME_TYPE = 'application/octet-stream'
 
+const tracer = new Tracer()
+const logger = new Logger()
+const metrics = new Metrics()
 const s3Client = new S3Client()
 
-export const handle = async (event, context) => {
+const getUploadUrl = ({ key, contentDisposition, contentDispositionHeader }) => {
+  const putCommand = new PutObjectCommand({
+    Bucket: BUCKET_NAME,
+    Key: key,
+    ContentDisposition: contentDisposition
+  })
+  const signableHeaders = new Set([`content-type: ${MIME_TYPE}`])
+  if (contentDisposition) {
+    signableHeaders.add(contentDispositionHeader)
+  }
+  return getSignedUrl(s3Client, putCommand, {
+    expiresIn: EXPIRY_DEFAULT,
+    signableHeaders
+  })
+}
+
+async function handler (event, context) {
   const id = randomUUID()
   const key = `shares/${id[0]}/${id[1]}/${id}`
 
@@ -18,30 +47,54 @@ export const handle = async (event, context) => {
   const contentDispositionHeader =
     contentDisposition && `content-disposition: ${contentDisposition}`
 
+  logger.info('Creating share', {
+    id,
+    key,
+    filename,
+    contentDispositionHeader
+  })
+  metrics.addMetric('createShare', MetricUnits.Count, 1)
+
   const downloadUrl = `${BASE_URL}/share/${id}`
+  const uploadUrl = await getUploadUrl({ key, contentDisposition, contentDispositionHeader })
 
-  const putCommand = new PutObjectCommand({
-    Bucket: BUCKET_NAME,
-    Key: key,
-    ContentDisposition: contentDisposition
-  })
+  let body = `
+  Upload with: curl -X PUT -T ${filename || '<FILENAME>'} ${
+    contentDispositionHeader ? `-H '${contentDispositionHeader}'` : ''
+  } '${uploadUrl}'
+  
+  Download with: curl ${downloadUrl}
+  `
+  let headers = { 'content-type': 'text/plain' }
 
-  const signableHeaders = new Set([`content-type: ${MIME_TYPE}`])
-  if (contentDisposition) {
-    signableHeaders.add(contentDispositionHeader)
+  if (event.preferredMediaType === 'application/json') {
+    body = JSON.stringify({
+      filename,
+      headers: [contentDispositionHeader],
+      uploadUrl,
+      downloadUrl
+    })
+    headers = { 'content-type': 'application/json' }
   }
-
-  const uploadUrl = await getSignedUrl(s3Client, putCommand, {
-    expiresIn: EXPIRY_DEFAULT,
-    signableHeaders
-  })
 
   return {
     statusCode: httpStatus.CREATED,
-    body: `
-    Upload with: curl -X PUT -T ${filename || '<FILENAME>'} ${contentDispositionHeader ? `-H '${contentDispositionHeader}'` : ''} '${uploadUrl}'
-    
-    Download with: curl ${downloadUrl}
-    `
+    headers,
+    body
   }
 }
+
+export const handle = middy(handler)
+  .use(injectLambdaContext(logger), { logEvents: true })
+  .use(logMetrics(metrics))
+  .use(captureLambdaHandler(tracer))
+  .use(httpHeaderNormalizer())
+  .use(
+    httpContentNegotiation({
+      parseCharsets: false,
+      parseEncodings: false,
+      parseLanguages: false,
+      failOnMismatch: false,
+      availableMediaTypes: ['text/plain', 'application/json']
+    })
+  )
